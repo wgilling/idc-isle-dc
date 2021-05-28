9457d1c0e39e80a0028c6e4fb0e41618ce71f725
Bethany Seeger <bseeger@jhu.edu>
2021-05-28 16:52:55 -0400

    Fixed bad url

--- tests/20-export-tests/testcafe/roles.js
+++ tests/20-export-tests/testcafe/roles.js
@@ -3,7 +3,7 @@ import { Role } from 'testcafe';
 /**
  * Drupal administrator via local login
  */
-export const adminUser = Role('https://islandora-idc-192-168-154-181.traefik.me/user/login', async t => {
+export const adminUser = Role('https://islandora-idc.traefik.me/user/login', async t => {
     await t
         .typeText('#edit-name', 'admin')
         .typeText('#edit-pass', 'password')
